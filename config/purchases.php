cbcd39f3362f5b0d08614f65832d4a233ff20419
imdhemy <imdhemy@gmail.com>
2020-06-25 06:14:12 +0200

    Update purchases.php

--- config/purchases.php
+++ config/purchases.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'google_app_credentials' => env('GOOGLE_APPLICATION_CRED', null),
-    'google_play_package' => env('GOOGLE_PLAY_PACKAGE', storage_path('google-app-credentials.json')),
+    'google_app_credentials' => env('GOOGLE_APPLICATION_CRED', storage_path('google-app-credentials.json')),
+    'google_play_package' => env('GOOGLE_PLAY_PACKAGE', 'example.com'),
 ];
