b7bdf86bb8a158f08d691cb78f38180832d9f8c5
imdhemy <imdhemy@gmail.com>
2020-08-25 13:09:03 +0200

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,10 +7,10 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
-    const GOOGLE_PLAY_PACKAGE = 'purchases.google_play_package';
-    const SUBSCRIPTION_ID = 'purchases.subscription_id';
-    const PURCHASE_TOKEN = 'purchases.purchase_token';
-    const GOOGLE_APP_CREDENTIALS = 'purchases.google_app_credentials';
+    const GOOGLE_PLAY_PACKAGE = 'com.twigano.fashion';
+    const SUBSCRIPTION_ID = 'week_premium';
+    const PURCHASE_TOKEN = 'ghpmfmednnbjkcheljjpdnbn.AO-J1OzOqWsD57dURPVrKYh2Qv-t5Lx9VJtCFLdxMovzAgfdF1CwX35AbH3RYRhAMqApdlgLvw7v1Eog43rWYGhGXODl9_Ir9O2YqcXqLSPM7ojuVr9mpcmUha2LZf3YaCbowk1UJfuc';
+    const GOOGLE_APP_CREDENTIALS_JSON = 'google-app-credentials.json';
 
     public function setUp(): void
     {
@@ -35,10 +35,10 @@ class TestCase extends Orchestra
             'prefix' => '',
         ]);
 
-        $app['config']->set(self::GOOGLE_PLAY_PACKAGE, 'com.twigano.fashion');
-        $app['config']->set(self::SUBSCRIPTION_ID . '', 'week_premium');
-        $app['config']->set(self::PURCHASE_TOKEN, 'ghpmfmednnbjkcheljjpdnbn.AO-J1OzOqWsD57dURPVrKYh2Qv-t5Lx9VJtCFLdxMovzAgfdF1CwX35AbH3RYRhAMqApdlgLvw7v1Eog43rWYGhGXODl9_Ir9O2YqcXqLSPM7ojuVr9mpcmUha2LZf3YaCbowk1UJfuc');
-        $app['config']->set(self::GOOGLE_APP_CREDENTIALS, 'google-app-credentials.json');
+        $app['config']->set('purchases.google_play_package', self::GOOGLE_PLAY_PACKAGE);
+        $app['config']->set('purchases.subscription_id', self::SUBSCRIPTION_ID);
+        $app['config']->set('purchases.purchase_token', self::PURCHASE_TOKEN);
+        $app['config']->set('purchases.google_app_credentials', self::GOOGLE_APP_CREDENTIALS_JSON);
 
         include_once __DIR__ . '/../database/migrations/create_purchase_logs_table.php.stub';
         (new \CreatePurchaseLogsTable())->up();
