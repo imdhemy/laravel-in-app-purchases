7fc96a75bf0fc55361073b73ed0d82aab8bf412d
imdhemy <imdhemy@gmail.com>
2020-10-30 00:32:10 +0200

    Update DeveloperNotificationControllerTest.php

--- tests/Http/Controllers/DeveloperNotificationControllerTest.php
+++ tests/Http/Controllers/DeveloperNotificationControllerTest.php
@@ -3,6 +3,7 @@
 namespace Imdhemy\Purchases\Tests\Http\Controllers;
 
 use Illuminate\Support\Facades\Event;
+use Imdhemy\Purchases\Events\GooglePlay\SubscriptionExpired;
 use Imdhemy\Purchases\Events\GooglePlay\SubscriptionPurchased;
 use Imdhemy\Purchases\Tests\TestCase;
 
@@ -29,6 +30,24 @@ class DeveloperNotificationControllerTest extends TestCase
     /**
      * @test
      */
+    public function test_google_subscription_expired_notification()
+    {
+        Event::fake();
+        $this->withoutExceptionHandling();
+        $data = [
+            'message' => [
+                'data' => 'eyJ2ZXJzaW9uIjoiMS4wIiwicGFja2FnZU5hbWUiOiJjb20udHdpZ2Fuby5mYXNoaW9uIiwiZXZlbnRUaW1lTWlsbGlzIjoiMTYwNDAwMjg0MjMzMiIsInN1YnNjcmlwdGlvbk5vdGlmaWNhdGlvbiI6eyJ2ZXJzaW9uIjoiMS4wIiwibm90aWZpY2F0aW9uVHlwZSI6MTMsInB1cmNoYXNlVG9rZW4iOiJuYWRpZmJwZWtmZmRjYmNvZGdwa2NrYWMuQU8tSjFPekxnU0ZQSWFESmVKTVF4dnZIYnBMeTlLZ3dYbHVyQjk1UlBINHFZdGYxSmdzV1B3NHV4bmlkYUlmeGJreXVpTDVOZ3ZudVU3TEpvNzIzeHpfVVRhUEZXc0YyZEEiLCJzdWJzY3JpcHRpb25JZCI6Im1vbnRoX3ByZW1pdW0ifX0=',
+            ],
+        ];
+        $uri = route('purchase.developerNotifications.google');
+        $this->post($uri, $data)->assertStatus(200);
+
+        Event::assertDispatched(SubscriptionExpired::class);
+    }
+
+    /**
+     * @test
+     */
     public function test_google_test_notification()
     {
         file_put_contents(storage_path('logs/laravel.log'), "");
@@ -42,7 +61,7 @@ class DeveloperNotificationControllerTest extends TestCase
         $this->post($uri, $data)->assertStatus(200);
 
         $this->assertTrue(
-            ! empty(file_get_contents(storage_path("/logs/laravel.log")))
+            !empty(file_get_contents(storage_path("/logs/laravel.log")))
         );
     }
 }
