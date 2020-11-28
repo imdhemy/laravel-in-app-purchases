a27a10b4eef098609bc17a36518ccf222ccec78d
imdhemy <imdhemy@gmail.com>
2020-11-28 15:27:17 +0200

    Implement AppStore server notification

---
+++ src/ServerNotifications/AppStoreServerNotification.php
@@ -0,0 +1,50 @@
+<?php
+
+
+namespace Imdhemy\Purchases\ServerNotifications;
+
+use Imdhemy\AppStore\ServerNotifications\ServerNotification;
+use Imdhemy\Purchases\Contracts\ServerNotificationContract;
+use Imdhemy\Purchases\Contracts\SubscriptionContract;
+use Imdhemy\Purchases\Subscriptions\AppStoreSubscription;
+
+class AppStoreServerNotification implements ServerNotificationContract
+{
+    /**
+     * @var ServerNotification
+     */
+    private $notification;
+
+    /**
+     * AppStoreServerNotification constructor.
+     * @param ServerNotification $notification
+     */
+    public function __construct(ServerNotification $notification)
+    {
+        $this->notification = $notification;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType(): string
+    {
+        return $this->notification->getNotificationType();
+    }
+
+    /**
+     * @return SubscriptionContract
+     */
+    public function getSubscription(): SubscriptionContract
+    {
+        return new AppStoreSubscription($this->notification->getUnifiedReceipt()->getLatestReceiptInfo());
+    }
+
+    /**
+     * @return bool
+     */
+    public function isTest(): bool
+    {
+        return false;
+    }
+}
---
+++ tests/ServerNotifications/AppStoreServerNotificationTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Imdhemy\Purchases\Tests\ServerNotifications;
+
+use Imdhemy\AppStore\ServerNotifications\ServerNotification;
+use Imdhemy\Purchases\Contracts\ServerNotificationContract;
+use Imdhemy\Purchases\Contracts\SubscriptionContract;
+use Imdhemy\Purchases\ServerNotifications\AppStoreServerNotification;
+use Imdhemy\Purchases\Tests\TestCase;
+
+class AppStoreServerNotificationTest extends TestCase
+{
+    /**
+     * @var AppStoreServerNotification
+     */
+    private $appStoreServerNotification;
+
+    /**
+     * @inheritDoc
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+        $path = realpath(__DIR__ . '/../appstore-server-notification.json');
+        $serverNotificationBody = json_decode(file_get_contents($path), true);
+
+        $serverNotification = ServerNotification::fromArray($serverNotificationBody);
+        $this->appStoreServerNotification = new AppStoreServerNotification($serverNotification);
+    }
+
+    /**
+     * @test
+     */
+    public function test_constructor()
+    {
+        $this->assertInstanceOf(ServerNotificationContract::class, $this->appStoreServerNotification);
+    }
+
+    /**
+     * @test
+     */
+    public function test_get_notification_type()
+    {
+        $this->assertEquals(
+            ServerNotification::DID_CHANGE_RENEWAL_STATUS,
+            $this->appStoreServerNotification->getType()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function test_get_subscription()
+    {
+        $this->assertInstanceOf(SubscriptionContract::class, $this->appStoreServerNotification->getSubscription());
+    }
+}
