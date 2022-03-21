033652da8ad12be8c3353b77b45b941dc93cb929
Mohamad Eldhemy <imdhemy@gmail.com>
2022-03-21 16:27:03 +0300

    chore: update code quality tools

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,43 @@
+<?php
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
+$includedPatterns = [
+    __DIR__ . '/src',
+    __DIR__ . '/tests',
+];
+
+$rules = [
+    '@PSR12' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'ordered_imports' => ['sort_algorithm' => 'alpha'],
+    'no_unused_imports' => true,
+    'not_operator_with_successor_space' => true,
+    'trailing_comma_in_multiline' => true,
+    'phpdoc_scalar' => true,
+    'unary_operator_spaces' => true,
+    'binary_operator_spaces' => true,
+    'blank_line_before_statement' => [
+        'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
+    ],
+    'phpdoc_single_line_var_spacing' => true,
+    'phpdoc_var_without_name' => true,
+    'method_argument_space' => [
+        'on_multiline' => 'ensure_fully_multiline',
+        'keep_multiple_spaces_after_comma' => true,
+    ]
+];
+
+$finder = Finder::create()
+    ->in($includedPatterns)
+    ->name('*.php')
+    ->notName('.*.blade.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+$config = new Config();
+$config->setRules($rules);
+$config->setFinder($finder);
+
+return $config;
