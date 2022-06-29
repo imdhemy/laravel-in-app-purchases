7f01f3fe045004e7704c2b129852535ac296cbce
Mohamad Eldhemy <imdhemy@gmail.com>
2022-06-29 16:35:29 +0300

    feat(console): implement publish config command

---
+++ src/Console/LiapConfigPublishCommand.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Imdhemy\Purchases\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+use Imdhemy\Purchases\ServiceProviders\LiapServiceProvider;
+
+/**
+ * Config publish command
+ *
+ * This command is used to publish LIAP configuration file
+ */
+class LiapConfigPublishCommand extends Command
+{
+    public const MESSAGE_ALREADY_INSTALLED = "purchase.php is already published.";
+
+    public const MESSAGE_SUCCESS = "purchase.php published successfully";
+
+    /**
+     * @inheritdoc
+     */
+    protected $signature = "liap:config:publish {--f|force}";
+
+    /**
+     * @inheritdoc
+     */
+    protected $description = "Publishes the LIAP configuration file.";
+
+    /**
+     * Executes the console command
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        if ($this->shouldForce()) {
+            return $this->publishConfig(true);
+        }
+
+        if ($this->isInstalled()) {
+            return $this->fail();
+        }
+
+        return $this->publishConfig();
+    }
+
+    /**
+     * Checks if the command should force publish the configs
+     *
+     * @return bool
+     */
+    private function shouldForce(): bool
+    {
+        return (bool)$this->option('force');
+    }
+
+    /**
+     * Publish configurations
+     *
+     * @param bool $force
+     *
+     * @return int
+     */
+    private function publishConfig(bool $force = false): int
+    {
+        $params = [
+            '--provider' => LiapServiceProvider::class,
+            '--tag' => 'config',
+        ];
+
+        if ($force) {
+            $params['--force'] = true;
+        }
+
+        $result = $this->call('vendor:publish', $params);
+        $this->info(self::MESSAGE_SUCCESS);
+
+        return $result;
+    }
+
+    /**
+     * Check if the freya is installed
+     *
+     * @return bool
+     */
+    private function isInstalled(): bool
+    {
+        return File::exists(config_path('purchase.php'));
+    }
+
+    /**
+     * Should run on command failure.
+     *
+     * @return int
+     */
+    private function fail(): int
+    {
+        $this->error(self::MESSAGE_ALREADY_INSTALLED);
+
+        return 1;
+    }
+}
---
+++ tests/Console/LiapConfigPublishCommandTest.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Tests\Console;
+
+use Illuminate\Support\Facades\File;
+use Imdhemy\Purchases\Console\LiapConfigPublishCommand;
+use Tests\TestCase;
+
+class LiapConfigPublishCommandTest extends TestCase
+{
+    /**
+     * @var string
+     */
+    private $configFilePath;
+
+    /**
+     * @inheritdoc
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->configFilePath = config_path('purchase.php');
+    }
+
+    /**
+     * @test
+     */
+    public function it_copies_the_configuration_file()
+    {
+        if (File::exists($this->configFilePath)) {
+            unlink($this->configFilePath);
+        }
+
+        $this->artisan('liap:config:publish')
+            ->expectsOutput(LiapConfigPublishCommand::MESSAGE_SUCCESS)
+            ->assertExitCode(0);
+
+        $this->assertFileExists($this->configFilePath);
+    }
+
+    /**
+     * @test
+     */
+    public function it_fails_if_the_configuration_file_already_exists()
+    {
+        if (File::exists($this->configFilePath)) {
+            unlink($this->configFilePath);
+        }
+
+        $command = 'liap:config:publish';
+
+        $this->artisan($command);
+
+        $this->artisan($command)
+            ->expectsOutput(LiapConfigPublishCommand::MESSAGE_ALREADY_INSTALLED)
+            ->assertExitCode(1);
+    }
+
+    /**
+     * @test
+     */
+    public function it_could_be_forced()
+    {
+        if (File::exists($this->configFilePath)) {
+            unlink($this->configFilePath);
+        }
+
+        $command = 'liap:config:publish --force';
+
+        $this->artisan($command);
+
+        $this->artisan($command)
+            ->expectsOutput(LiapConfigPublishCommand::MESSAGE_SUCCESS)
+            ->assertExitCode(0);
+    }
+}
