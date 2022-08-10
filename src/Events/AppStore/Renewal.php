777ca6e797331a2bb583d279a942db1857cdebbd
Mohamad Eldhemy <imdhemy@gmail.com>
2022-08-10 14:57:49 +0300

    chore(app-store): deprecate renewal event

--- src/Events/AppStore/Renewal.php
+++ src/Events/AppStore/Renewal.php
@@ -4,6 +4,10 @@ namespace Imdhemy\Purchases\Events\AppStore;
 
 use Imdhemy\Purchases\Events\PurchaseEvent;
 
+/**
+ * @deprecated use \Imdhemy\Purchases\Events\AppStore\DidRecover instead
+ * @see        \Imdhemy\Purchases\Events\AppStore\DidRecover
+ */
 class Renewal extends PurchaseEvent
 {
 }
