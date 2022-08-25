40ee35e58afc2f36138fea3994615d2eb4602899
Mohamad Eldhemy <imdhemy@gmail.com>
2022-08-25 11:00:26 +0300

    chore(routes): fix line length

--- routes/routes.php
+++ routes/routes.php
@@ -3,4 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use Imdhemy\Purchases\Http\Controllers\ServerNotificationController;
 
-Route::post('/liap/notifications', ServerNotificationController::class)->name('liap.serverNotifications');
+Route::post('/liap/notifications', ServerNotificationController::class)
+    ->name('liap.serverNotifications');
