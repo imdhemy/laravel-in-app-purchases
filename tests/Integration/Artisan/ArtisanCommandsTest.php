a48fe8a3ac58a6efb3ea595de599310072677634
Mohamad Eldhemy <imdhemy@gmail.com>
2022-08-26 08:45:00 +0300

    chore(tests): url command prints table of URLs

--- tests/Integration/Artisan/ArtisanCommandsTest.php
+++ tests/Integration/Artisan/ArtisanCommandsTest.php
@@ -4,8 +4,12 @@ namespace Tests\Integration\Artisan;
 
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Str;
 use Imdhemy\Purchases\Console\LiapConfigPublishCommand;
+use Imdhemy\Purchases\Console\LiapUrlCommand;
+use Tests\Doubles\UrlGeneratorDouble;
 use Tests\TestCase;
 
 /**
@@ -27,6 +31,7 @@ class ArtisanCommandsTest extends TestCase
      * @inheritDoc
      * - Gets all artisan commands
      * - Deletes the configuration file if it exists
+     * - Enables signed URLs
      */
     protected function setUp(): void
     {
@@ -38,19 +43,21 @@ class ArtisanCommandsTest extends TestCase
         if (File::exists($this->configFilePath)) {
             unlink($this->configFilePath);
         }
+
+        config()->set('liap.routing.signed', true);
     }
 
     /**
      * @test
      */
-    public function it_should_provide_publish_config_command(): void
+    public function it_should_provide_config_publish_command(): void
     {
         $this->assertArrayHasKey('liap:config:publish', $this->commands);
     }
 
     /**
      * @test
-     * @depends it_should_provide_publish_config_command
+     * @depends it_should_provide_config_publish_command
      */
     public function publish_config_command_should_publish_the_config_file(): void
     {
@@ -67,5 +74,38 @@ class ArtisanCommandsTest extends TestCase
     public function it_should_provide_url_command(): void
     {
         $this->assertArrayHasKey('liap:url', $this->commands);
+    }
+
+    /**
+     * @test
+     * @depends it_should_provide_url_command
+     */
+    public function url_command_prints_table_of_provider_endpoints(): void
+    {
+        $answers = [
+            LiapUrlCommand::PROVIDER_ALL,
+            LiapUrlCommand::PROVIDER_APP_STORE,
+            LiapUrlCommand::PROVIDER_GOOGLE_PLAY,
+        ];
+
+        /** @var UrlGeneratorDouble $urlGenerator */
+        $urlGenerator = $this->app->make(UrlGeneratorDouble::class);
+        $providers = [LiapUrlCommand::PROVIDER_APP_STORE, LiapUrlCommand::PROVIDER_GOOGLE_PLAY];
+        $expected = new Collection();
+
+        foreach ($providers as $provider) {
+            $expected->add([
+                $provider,
+                $urlGenerator->generate((string)Str::of($provider)->slug()),
+            ]);
+        }
+
+        $this->artisan('liap:url')
+            ->expectsChoice(
+                LiapUrlCommand::CHOICE_PROVIDER,
+                LiapUrlCommand::PROVIDER_ALL,
+                $answers
+            )->expectsTable(LiapUrlCommand::TABLE_HEADERS, $expected->toArray())
+            ->assertSuccessful();
     }
 }
