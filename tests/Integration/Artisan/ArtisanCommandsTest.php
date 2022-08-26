20eeb38fdb889c37e1e78ee426a94aa94e233a3a
Mohamad Eldhemy <imdhemy@gmail.com>
2022-08-26 08:14:05 +0300

    chore(tests): test liap:config:publish command
    - it creates the `config/liap.php` file

--- tests/Integration/Artisan/ArtisanCommandsTest.php
+++ tests/Integration/Artisan/ArtisanCommandsTest.php
@@ -4,23 +4,40 @@ namespace Tests\Integration\Artisan;
 
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Support\Facades\File;
+use Imdhemy\Purchases\Console\LiapConfigPublishCommand;
 use Tests\TestCase;
 
+/**
+ * The word `it` refers to LIAP package
+ */
 class ArtisanCommandsTest extends TestCase
 {
     /**
      * @var array<string, Command>
      */
-    private $commands;
+    private array $commands;
+
+    /**
+     * @var string The path to the configuration file `config/liap.php`
+     */
+    private string $configFilePath;
 
     /**
      * @inheritDoc
+     * - Gets all artisan commands
+     * - Deletes the configuration file if it exists
      */
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->commands = $this->app->make(Kernel::class)->all();
+
+        $this->configFilePath = config_path('liap.php');
+        if (File::exists($this->configFilePath)) {
+            unlink($this->configFilePath);
+        }
     }
 
     /**
@@ -29,6 +46,19 @@ class ArtisanCommandsTest extends TestCase
     public function it_should_provide_publish_config_command(): void
     {
         $this->assertArrayHasKey('liap:config:publish', $this->commands);
+    }
+
+    /**
+     * @test
+     * @depends it_should_provide_publish_config_command
+     */
+    public function publish_config_command_should_publish_the_config_file(): void
+    {
+        $this->artisan('liap:config:publish')
+            ->expectsOutput(LiapConfigPublishCommand::MESSAGE_SUCCESS)
+            ->assertExitCode(0);
+
+        $this->assertFileExists($this->configFilePath);
     }
 
     /**
