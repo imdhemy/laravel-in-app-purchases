5a2ac44c99bad14ae05ffeea98085f2c529b9413
Mohamad Eldhemy <imdhemy@gmail.com>
2022-09-22 20:56:34 +0200

    feat(handler): create app-store v2 handler

---
+++ src/Handlers/AppStoreV2NotificationHandler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Imdhemy\Purchases\Handlers;
+
+class AppStoreV2NotificationHandler extends AbstractNotificationHandler
+{
+    /**
+     * @return mixed
+     */
+    protected function handle()
+    {
+    }
+}
