181e1b0e6fce4fc37c0a8fc9d85e674f697e128e
Mohamad Eldhemy <imdhemy@gmail.com>
2022-11-01 15:48:38 +0200

    feat(apple): request a test notification

---
+++ src/Services/AppStoreTestNotificationService.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imdhemy\Purchases\Services;
+
+use GuzzleHttp\Exception\GuzzleException;
+use Imdhemy\AppStore\ServerNotifications\TestNotificationService;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * This class is used to request a test notification from Apple
+ */
+final class AppStoreTestNotificationService
+{
+    /**
+     * @var TestNotificationService
+     */
+    private TestNotificationService $service;
+
+    /**
+     * @param TestNotificationService $service
+     */
+    public function __construct(TestNotificationService $service)
+    {
+        $this->service = $service;
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function request(): ResponseInterface
+    {
+        return $this->service->request();
+    }
+}
