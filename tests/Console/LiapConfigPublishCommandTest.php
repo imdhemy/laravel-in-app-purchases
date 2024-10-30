43c2d5de4074cbb3c5dcad6b3d2f4582c22a0797
imdhemy <imdhemy@gmail.com>
2024-10-30 20:34:01 +0100

    chore(tests): code refactoring

--- tests/Console/LiapConfigPublishCommandTest.php
+++ tests/Console/LiapConfigPublishCommandTest.php
@@ -5,70 +5,55 @@ declare(strict_types=1);
 namespace Imdhemy\Purchases\Tests\Console;
 
 use Illuminate\Support\Facades\File;
-use Imdhemy\Purchases\Console\LiapConfigPublishCommand;
 use Imdhemy\Purchases\Tests\TestCase;
 
-class LiapConfigPublishCommandTest extends TestCase
+final class LiapConfigPublishCommandTest extends TestCase
 {
     private string $configFilePath;
+    private string $config;
 
     protected function setUp(): void
     {
         parent::setUp();
 
         $this->configFilePath = config_path('liap.php');
-    }
+        $this->config = file_get_contents(__DIR__.'/../../config/liap.php');
 
-    /**
-     * @test
-     */
-    public function it_should_publish_liap_config_file(): void
-    {
         if (File::exists($this->configFilePath)) {
             unlink($this->configFilePath);
         }
+    }
 
-        $this->artisan('liap:config:publish')
-            ->expectsOutput(LiapConfigPublishCommand::MESSAGE_SUCCESS)
-            ->assertSuccessful();
+    /**
+     * @test
+     */
+    public function publish_config_file(): void
+    {
+        $this->artisan('liap:config:publish')->assertSuccessful();
 
         $this->assertFileExists($this->configFilePath);
+        $this->assertSame($this->config, file_get_contents($this->configFilePath));
     }
 
     /**
      * @test
-     *
-     * @depends it_should_publish_liap_config_file
      */
-    public function it_should_copy_the_contents_of_liap_config_file(): void
+    public function it_should_file_if_config_is_published(): void
     {
-        $expected = file_get_contents(__DIR__.'/../../config/liap.php');
-        $actual = file_get_contents($this->configFilePath);
+        $this->artisan('liap:config:publish');
 
-        $this->assertEquals($expected, $actual);
+        $this->artisan('liap:config:publish')->assertFailed();
     }
 
     /**
      * @test
-     *
-     * @depends it_should_copy_the_contents_of_liap_config_file
      */
-    public function it_should_fail_if_liap_config_file_already_published(): void
+    public function publishing_config_file_can_be_forced(): void
     {
-        $this->artisan('liap:config:publish')
-            ->expectsOutput(LiapConfigPublishCommand::MESSAGE_ALREADY_INSTALLED)
-            ->assertFailed();
-    }
+        $this->artisan('liap:config:publish');
 
-    /**
-     * @test
-     *
-     * @depends it_should_fail_if_liap_config_file_already_published
-     */
-    public function it_should_publish_liap_config_file_when_force_is_passed(): void
-    {
-        $this->artisan('liap:config:publish --force')
-            ->expectsOutput(LiapConfigPublishCommand::MESSAGE_SUCCESS)
-            ->assertSuccessful();
+        $this->artisan('liap:config:publish --force')->assertSuccessful();
+        $this->assertFileExists($this->configFilePath);
+        $this->assertSame($this->config, file_get_contents($this->configFilePath));
     }
 }
