f1c88155f61c448fe73596564303f208a2a9117f
Mehmet Onur AKKAYA <monurakkaya@gmail.com>
2025-05-19 22:01:05 +0300

    fix(app-store): fixes test notification uri constants

--- src/Services/AppStoreTestNotificationServiceBuilder.php
+++ src/Services/AppStoreTestNotificationServiceBuilder.php
@@ -109,7 +109,7 @@ class AppStoreTestNotificationServiceBuilder
 
     protected function createClient(): ClientInterface
     {
-        $baseURI = $this->sandbox ? ClientFactory::STORE_KIT_SANDBOX_URI : ClientFactory::STORE_KIT_PRODUCTION_URI;
+        $baseURI = $this->sandbox ? ClientFactory::BASE_URI_SANDBOX : ClientFactory::BASE_URI;
 
         return ClientFactory::create($this->sandbox, ['base_uri' => $baseURI]);
     }
