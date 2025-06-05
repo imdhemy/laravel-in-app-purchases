72afb0cdd34ff3d12aac12004a8960b57fceaec1
Dhemy <imdhemy@gmail.com>
2025-06-05 19:24:11 +0300

    chore(security): remove code related to older laravel versions

--- src/Console/UrlGenerator.php
+++ src/Console/UrlGenerator.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Imdhemy\Purchases\Console;
 
-use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Http\Request;
 use Illuminate\Routing\UrlGenerator as LaravelUrlGenerator;
-use Illuminate\Support\Str;
 use Imdhemy\Purchases\Contracts\UrlGenerator as UrlGeneratorContract;
 
 /**
@@ -16,12 +14,10 @@ use Imdhemy\Purchases\Contracts\UrlGenerator as UrlGeneratorContract;
 class UrlGenerator implements UrlGeneratorContract
 {
     private LaravelUrlGenerator $urlGenerator;
-    private Application $app;
 
-    public function __construct(LaravelUrlGenerator $urlGenerator, Application $app)
+    public function __construct(LaravelUrlGenerator $urlGenerator)
     {
         $this->urlGenerator = $urlGenerator;
-        $this->app = $app;
     }
 
     public function signedUrl(string $provider): string
@@ -43,50 +39,7 @@ class UrlGenerator implements UrlGeneratorContract
         return $this->signedUrl($provider);
     }
 
-    /**
-     * This method returns true if and only if the given URL is a valid signed URL
-     * It's used to provide the same functionality as the Laravel UrlGenerator v.9
-     * which allows to ignore specific query parameters when validating the URL.
-     *
-     * {@inheritDoc}
-     */
     public function hasValidSignature(Request $request): bool
-    {
-        if ($this->shouldDelegateToLaravel()) {
-            return $this->validateByLaravel($request);
-        }
-
-        $ignoreQuery = ['signature', 'provider'];
-        $url = $request->url();
-
-        $queryString = collect(explode('&', (string)$request->server->get('QUERY_STRING')))
-            ->reject(function (string $parameter) use ($ignoreQuery) {
-                return in_array(Str::before($parameter, '='), $ignoreQuery);
-            })
-            ->join('&');
-
-        $original = rtrim($url.'?'.$queryString, '?');
-        $signature = hash_hmac('sha256', $original, (string)config('app.key'));
-
-        /** @var string $signatureQuery */
-        $signatureQuery = $request->query('signature', '');
-
-        return hash_equals($signature, $signatureQuery);
-    }
-
-    private function shouldDelegateToLaravel(): bool
-    {
-        return version_compare($this->app->version(), '9', '>=');
-    }
-
-    /**
-     * This method is only used when Laravel is >= 9.0.0
-     * Starting from Laravel 9.0.0, the `hasValidSignature` method allows to
-     * pass query names to be ignored when validating the signature.
-     *
-     * @psalm-suppress TooManyArguments
-     */
-    private function validateByLaravel(Request $request): bool
     {
         return $this->urlGenerator->hasValidSignature($request, true, ['provider']);
     }
